79b7584a1bdb9847e9935add555957ec4e63e404
yeongpyo <byp93@naver.com>
2018-04-29 23:22:02 +0900

    2018-04-29-23:21 base layout Setting

--- app/src/main/java/com/example/yeongpyo/shopping_app/MainActivity.kt
+++ app/src/main/java/com/example/yeongpyo/shopping_app/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.yeongpyo.shopping_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var Toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(this,drawerlayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+        drawerlayout.setDrawerListener(Toggle)
+        Toggle.syncState()
+
     }
 }
